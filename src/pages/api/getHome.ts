cf8b83c6e82aec26bc67bea8278278ecf758591d
traysonkelii <traysonkelii@gmail.com>
2023-02-12 02:09:54 -0700

    updated errors

--- src/pages/api/getHome.ts
+++ src/pages/api/getHome.ts
@@ -1,7 +1,7 @@
 import { GoogleAuth } from 'google-auth-library';
 import { google } from 'googleapis';
 
-export default async (req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { data: any[][] | null | undefined; }): void; new(): any; }; }; }) => {
+export const getHome = async (req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { data: any[][] | null | undefined; }): void; new(): any; }; }; }) => {
   const auth = new GoogleAuth({
     keyFile: 'secrets.json',
     scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,15 @@
 import { Banner, BannerGradient } from "@/components/Banner";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Image from "next/image";
 import logo from "../../../public/images/early.svg";
-import { SheetsTextParser } from "@/services/SheetsParser/SheetsTextParser";
-import { SheetsBannerParser } from "@/services/SheetsParser/SheetsBannerParser";
 import { useSheetsContext } from "@/context/SheetsContext";
+import { useMediaQuery } from "react-responsive";
 
 const Home = () => {
-
-  const {homeContext} = useSheetsContext();
+  const isMobile = useMediaQuery({ maxWidth: 900 });
+  const { homeContext } = useSheetsContext();
 
   return (
     <>
@@ -31,11 +30,11 @@ const Home = () => {
             style={{ marginTop: "10%" }}
           >
             <p
-            // style={
-            //   isMobile
-            //     ? { margin: 0, padding: 0, fontSize: "small" }
-            //     : { margin: 0, padding: 0, fontSize: "large" }
-            // }
+              style={
+                isMobile
+                  ? { margin: 0, padding: 0, fontSize: "small" }
+                  : { margin: 0, padding: 0, fontSize: "large" }
+              }
             >
               Learn More
             </p>
@@ -59,8 +58,10 @@ const HomeButton = styled.button`
   color: #8cc640;
   background-color: white;
   border: 2px solid #8cc640;
-  padding-left: 3em;
-  padding-right: 3em;
+  padding-top: 1%;
+  padding-bottom: 1%;
+  padding-left: 6%;
+  padding-right: 6%;
   transition: all 0.5s;
   font-family: "Genrock-Regular", Helvetica;
   font-size: large;
@@ -68,6 +69,12 @@ const HomeButton = styled.button`
     color: white;
     background-color: #8cc640;
     cursor: pointer;
+  }
+  @media all and (max-width: 900px) {
+    margin-right: 5%;
+    margin-left: 5%;
+    margin-bottom: 5%;
+    font-size: small;
   }
 `;
 
