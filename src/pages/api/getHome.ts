7067f52d56b45d7dd94e0294d1160a5e299be2f1
traysonkelii <traysonkelii@gmail.com>
2023-02-20 10:20:56 -0700

    am i getting the right values?

--- src/pages/api/getHome.ts
+++ src/pages/api/getHome.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async (req: any, res: any) => {
 
-  const baseUrl = process.env.BASE_SHEETS_API_URL;
+  const baseUrl = 'https://49bpwej7qa.execute-api.us-east-1.amazonaws.com/prod/api/v1/sheets?';
   const queryString = 'range=home!A1:Z40';
 
   const sheetsResponse = await (await axios.get(baseUrl + queryString)).data;
