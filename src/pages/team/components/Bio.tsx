4c20fbcd02cc507008dffd16751d9fef9def148b
traysonkelii <traysonkelii@gmail.com>
2023-03-22 07:21:07 -0700

    fixing errors

--- src/pages/team/components/Bio.tsx
+++ src/pages/team/components/Bio.tsx
@@ -4,13 +4,13 @@ import React from "react";
 export const Bio = ({ name, title, desc }: SheetsBioData) => {
   return (
     <div style={{ textAlign: "left", marginTop: "50px" }}>
-      <p style={{ lineHeight: 1.5 }}>
+      {/* <p style={{ lineHeight: 1.5 }}>
         <strong>{name}</strong> <br /> <i>{title}</i>
       </p>
       {desc.map((text, index) => (
         <p dangerouslySetInnerHTML={{ __html: text }} key={index} />
       ))}
-      <hr />
+      <hr /> */}
     </div>
   );
 };
